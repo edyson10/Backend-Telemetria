529cc5a0a12a22129fba3334829fa16255bb92ca
edyson10 <edysonfabian@hotmail.com>
2026-09-08 08:43:06 -0500

    Feat: Se hace el primer commit

---
+++ src/main/java/com/movilidad/backendtelemetria/BackendTelemetriaApplication.java
@@ -0,0 +1,13 @@
+package com.movilidad.backendtelemetria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTelemetriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTelemetriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movilidad/backendtelemetria/BackendTelemetriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movilidad.backendtelemetria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTelemetriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
